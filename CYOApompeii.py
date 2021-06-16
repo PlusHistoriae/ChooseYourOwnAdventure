08c3341ddc7f480be539df2af17300308f056211
PlusHistoriae <85767310+PlusHistoriae@users.noreply.github.com>
2021-06-16 14:56:50 +0000

    This is Ravi's work

--- CYOApompeii.py
+++ CYOApompeii.py
@@ -1,5 +1,12 @@
 print ("Hello. This is the ChooseYourOwnAdventure speaking. You are playing ChooseYourOwnAdventure - Pompeii, an adventure inspired by the values shown by Pompeii. Can you stop the eruption, while fighting hellish beasts, or will you burn to a crisp in the volcanic ashes, along with all the rest of the city? Find out, with ChooseYourOwnAdventure - Pompeii.")
 
-print ("Would you like to proceed with ChooseYourOwnAdventure? If yes, type proceed. If no, type stop.")
+print ("Would you like to proceed with ChooseYourOwnAdventure? If yes, type 1. If no, type 2.")
+
 proceed_question = input()
-print('Your answer is: ' + proceed_question)
+proceed_question_answer_proceed = 1
+proceed_question_answer_stop = 2
+
+if str(proceed_question) == proceed_question_answer_stop:
+  print ("The CYOA has been stopped.")
+elif str(proceed_question) == proceed_question_answer_proceed:
+  print ("The CYOA shall proceed.")
