657fc67a254514c529b21157959adc34f0bcf9ce
PlusHistoriae <85767310+PlusHistoriae@users.noreply.github.com>
2021-06-16 18:06:12 +0000

    Big Update 1

--- CYOApompeii.py
+++ CYOApompeii.py
@@ -9,4 +9,13 @@ if proceed_question == '2':
 
 if proceed_question == '1':
   print ("The ChooseYourOwnAdventure shall proceed.")
-  print ("You will be reffered to as 'Thomas Brown', for the length of the program.
+  print ("You will be reffered to as Kim Oustil for the length of the program.")
+  print ("You walk down the streets of Pompeii, wondering, thinking, and procrastinating about your recent ousting. You were once part of the secret illegal society known as the 'Divine Hand', a religious group, with you as chancellor of the society, but the lowers ousted you, and you are now just a peasant, living off the streets. Ideas of revenge flow through your mind, but part of you wants them to prosper, maybe let them be, and after a while re-join the society. What will you do? Type 1 for revenge, and 2 for pacifism")
+
+choice_1a = input()
+
+if choice_1a == '1':
+  print ("ok")
+  
+if choice_1a == '2':
+  print("fine")
