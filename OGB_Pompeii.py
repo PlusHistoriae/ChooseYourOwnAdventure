7663485a6c873f5ccc3872ee97245e28b63ad11b
PlusHistoriae <85767310+PlusHistoriae@users.noreply.github.com>
2021-06-16 23:34:14 +0000

    Renaming

--- OGB_Pompeii.py
+++ OGB_Pompeii.py
@@ -1,14 +1,14 @@
-print ("Hello. This is the ChooseYourOwnAdventure speaking. You are playing ChooseYourOwnAdventure - Pompeii, an adventure inspired by the values shown by Pompeii. Can you stop the eruption, while fighting hellish beasts, or will you burn to a crisp in the volcanic ashes, along with all the rest of the city? Find out, with ChooseYourOwnAdventure - Pompeii.")
+print ("Hello. This is the OnlineGameBook speaking. You are playing OnlineGameBook - Pompeii, an adventure inspired by the values shown by Pompeii. Can you stop the eruption, while fighting hellish beasts, or will you burn to a crisp in the volcanic ashes, along with all the rest of the city? Find out, with OnlineGameBook - Pompeii.")
 
-print ("Would you like to proceed with ChooseYourOwnAdventure? If yes, type 1. If no, type 2.")
+print ("Would you like to proceed with OnlineGameBook? If yes, type 1. If no, type 2.")
 
 proceed_question = input()
 
 if proceed_question == '2':
-  print("You stopped the ChooseYourOwnAdventure")
+  print("You stopped the OnlineGameBook.")
 
 if proceed_question == '1':
-  print ("The ChooseYourOwnAdventure shall proceed.")
+  print ("The OnlineGameBook shall proceed.")
   print ("You will be reffered to as Kim Oustil for the length of the program.")
   print ("You walk down the streets of Pompeii, wondering, thinking, and procrastinating about your recent ousting. You were once part of the secret illegal society known as the 'Divine Hand', a religious group, with you as chancellor of the society, but the lowers ousted you, and you are now just a peasant, living off the streets. Ideas of revenge flow through your mind, but part of you wants them to prosper, maybe let them be, and after a while re-join the society. What will you do? Type 1 for revenge, and 2 for pacifism")
 
--- main.py
+++ main.py
@@ -1,2 +1,42 @@
-print ("Welcome to ChooseYourOwnAdventure, the fluid, yet random online choose your own adventure! For a different adventure, fork the project on GitHub, and change the import at https://github.com/PlusHistoriae/ChooseYourOwnAdventure.")
-import AgeTest
+import random
+
+print ("Welcome to OnlineGameBook, the fluid, yet random online gamebook genre type program! Please fill out the following question to deter 4 year olds.")
+
+numbers = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+operators = "*", "+"
+
+operand1 = random.choice(numbers)
+operand2 = random.choice(numbers)
+operator = random.choice(operators)
+
+if operator == '+':
+    answer = operand1 + operand2
+elif operator == '-':
+    answer = operand1 - operand2
+else:
+    answer = operand1 * operand2
+
+user_input = input(str(operand1) + operator + str(operand2) + "? ")
+
+if str(answer) == user_input:
+    print("Correct!")
+    print("Choose your gamebook! Your options are Pompeii (at the time of the eruption), Britian (at the time of the Renaissance, Germany (at the time of WW2, Russia (at the time of the Russian Revoloution), France (at the time of Napoleon), and the USA (at the time of the revoloution. Type the number in which they are chronologicallly listed (for example Pompeii 1, Britian 2, etc) for that specific OnlineGameBook!")
+else:
+    print("Wrong!")
+
+OGB_question = input()
+
+if OGB_question == '1':
+  import OGB_Pompeii
+elif OGB_question == '2':
+  import OGB_Britian
+elif OGB_question == '3':
+  import OGB_Germany
+elif OGB_question == '4':
+  import OGB_Russia
+elif OGB_question == '5':
+  import OGB_France
+elif OGB_question == '6':
+  import OGB_USA
+else:
+  print ("You have not entered a valid number/sentence/etc. If you wish to go forth, please reload the OnlineGameBook, and enter a valid number/sentence/etc.")
